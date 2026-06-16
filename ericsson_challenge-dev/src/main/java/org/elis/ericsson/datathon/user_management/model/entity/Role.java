d59b5babd8407e371795e3887a143fc22e3bb0e6
Angelo Gaggero <angelo.gaggero@ericsson.com>
2026-06-16 16:50:11 +0200

    fix(UC-B-002): remove @EnableAutoConfiguration from Role and RefreshToken entities, remove redundant @EntityListeners from Role, add entity annotation tests

--- ericsson_challenge-dev/src/main/java/org/elis/ericsson/datathon/user_management/model/entity/Role.java
+++ ericsson_challenge-dev/src/main/java/org/elis/ericsson/datathon/user_management/model/entity/Role.java
@@ -1,23 +1,17 @@
 package org.elis.ericsson.datathon.user_management.model.entity;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.elis.ericsson.datathon.user_management.model.modelbase.DateAudit;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@EnableAutoConfiguration
-@EntityListeners(AuditingEntityListener.class)
 public class Role extends DateAudit {
 
     @Id
